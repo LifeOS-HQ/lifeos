988a7a735a7c8d9ee420aafec762f36a55d492a1
Daniel Sundermeier <daniel@hof-sundermeier.de>
2021-01-06 07:04:50 +0100

    fixes deployment controller

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -17,7 +17,7 @@ class DeploymentController extends Controller
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
 
         $root_path = base_path();
-        $process = new Process('cd ' . $root_path . '; sh ./deploy.sh');
+        $process = Process::fromShellCommandline('cd ' . $root_path . '; sh ./deploy.sh');
         $process->run();
 
         // executes after the command finishes
