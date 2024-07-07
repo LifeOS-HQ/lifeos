80cd6976fe180dc63534734af187588b74c6663e
danielsundermeier <daniel@hof-sundermeier.de>
2024-07-07 05:38:57 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ php artisan migrate --force
 php artisan queue:restart
 composer install  > /dev/null 2>&1 & echo $! #--no-interaction --no-dev --prefer-dist
 npm install > /dev/null 2>&1 & echo $!
-npm run production > /dev/null 2>&1 & echo $!
+npm run development > /dev/null 2>&1 & echo $!
 
 echo "Deployed"
