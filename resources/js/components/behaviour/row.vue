9fd20e84d4515b1b3e78c2a1075ce399cb6c1ec0
danielsundermeier <daniel@hof-sundermeier.de>
2024-11-06 02:56:59 +0100

    behaviour: index

---
+++ resources/js/components/behaviour/row.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <show :item="item" :has-edit-button="false" @destroying="destroy()">
+
+    <template v-slot:show>
+
+        <td class="align-middle pointer" @click="show">{{ item.name }}</td>
+
+    </template>
+
+    </show>
+
+</template>
+
+<script>
+    import show from '../tables/rows/show.vue';
+
+    import { showMixin } from '../../mixins/tables/rows/show.js';
+
+    export default {
+
+        components: {
+            show,
+        },
+
+        mixins: [
+            showMixin,
+        ],
+
+        props: {
+            //
+        },
+
+        data () {
+            return {
+                //
+            };
+        },
+
+        methods: {
+            //
+        },
+
+    };
+</script>
--- resources/views/behaviour/index.blade.php
+++ resources/views/behaviour/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
+@section('headline', \App\Models\Behaviours\Behaviour::label())
+
 @section('content')
 
-
+<behaviour-table index-path="{{ \App\Models\Behaviours\Behaviour::indexPath() }}"></behaviour-table>
 
 @endsection
