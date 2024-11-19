8096e532bd7f1e1a919a04cfcdcab71c2e63e6ab
danielsundermeier <daniel@hof-sundermeier.de>
2024-11-19 02:32:33 +0100

    wahoo webhook import workout

--- app/Http/Controllers/Wahoo/WebhookController.php
+++ app/Http/Controllers/Wahoo/WebhookController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Wahoo;
 
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
+use App\Models\Services\Service;
+use App\Models\Workouts\History;
+use App\Models\Workouts\Workout;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 
@@ -12,9 +16,36 @@ class WebhookController extends Controller
     {
         $this->log($request);
 
+        $event_type = $request->input('event_type');
+
+        return match ($event_type) {
+            'workout_summary' => $this->handleWorkoutSummary($request->all()),
+            default => null,
+        };
+
         return [
             //
         ];
+    }
+
+    private function handleWorkoutSummary(array $data)
+    {
+        $service = Service::where('slug', 'wahoo')->first();
+
+        $service_user = \App\Models\Services\User::query()
+            ->where('service_id', $service->id)
+            ->where('service_user_id', $data['user']['id'])
+            ->first();
+
+        $workout = Workout::where('user_id', $service_user->user_id)
+            ->where('name', 'Zwift 750 kcal')
+            ->first();
+
+        $wahoo_workout = $data['workout_summary']['workout'];
+        Arr::forget($data, 'workout_summary.workout');
+        $wahoo_workout['workout_summary'] = $data['workout_summary'];
+
+        History::updateOrCreateFromWahoo($workout, $wahoo_workout);
     }
 
     private function log(Request $request)
